3daea3fa9a3a9637365ed62bccb1ef04947b8e5b
Sebastian <sebekz@gmail.com>
2022-06-03 10:26:21 +0200

    Collect links feature

---
+++ operations/CollectLinks.js
@@ -0,0 +1,157 @@
+const HttpOperation = require("./HttpOperation");
+const CompositeInjectMixin = require("./mixins/CompositeInjectMixin");
+const CompositeScrapeMixin = require("./mixins/CompositeScrapeMixin");
+// const Operation = require('./Operation')//For jsdoc
+var cheerio = require("cheerio");
+// var cheerioAdv = require('cheerio-advanced-selectors');
+// cheerio = cheerioAdv.wrap(cheerio);
+const {
+  getBaseUrlFromBaseTag,
+  createElementList,
+} = require("../utils/cheerio");
+const { getAbsoluteUrl } = require("../utils/url");
+const PageHelper = require("./helpers/PageHelper");
+// const SPA_PageHelper = require('./helpers/SPA_PageHelper');
+// const { CustomResponse } = require('../request/request');//For jsdoc
+const { mapPromisesWithLimitation } = require("../utils/concurrency");
+
+/**
+ *
+ * @mixes CompositeInjectMixin
+ * @mixes CompositeScrapeMixin
+ */
+class CollectLinks extends HttpOperation {
+  // This operation is responsible for collecting links in a given page. It does not fetch the links themselves!
+
+  /**
+   *
+   * @param {string} querySelector cheerio-advanced-selectors selector
+   * @param {Object} [config]
+   * @param {string} [config.name = 'Default OpenLinks name']
+   * @param {Object} [config.pagination = null] Look at the pagination API for more details.
+   * @param {number[]} [config.slice = null]
+   * @param {Function} [config.condition = null] Receives a Cheerio node.  Use this hook to decide if this node should be included in the scraping. Return true or false
+   * @param {Function} [config.getElementList = null] Receives an elementList array
+   * @param {Function} [config.getPageData = null]
+   * @param {Function} [config.getPageObject = null] Receives a dictionary of children, and an address argument
+   * @param {Function} [config.getPageResponse = null] Receives an axiosResponse object
+   * @param {Function} [config.getPageHtml = null] Receives htmlString and pageAddress
+   * @param {Function} [config.getException = null] Listens to every exception. Receives the Error object.
+   * @param {(href: string) => string} [config.transformHref = undefined] Callback that receives the href before it is opened.
+   *
+   */
+
+  constructor(querySelector, config) {
+    super(config);
+    // this.pageHelper = new PageHelper(this);
+    this.pageHelper = null;
+    // this.compositeHelper = new CompositeHelper(this);
+    // this.virtualOperations = []
+    this.querySelector = querySelector;
+
+    if (
+      typeof config === "object" &&
+      typeof config.transformHref === "function"
+    ) {
+      this.transformHref = config.transformHref;
+    } else {
+      this.transformHref = function (href) {
+        return href;
+      };
+    }
+  }
+
+  initPageHelper() {
+    if (!this.scraper.config.usePuppeteer) {
+      this.pageHelper = new PageHelper(this);
+    } else {
+      this.pageHelper = new SPA_PageHelper(this);
+    }
+  }
+
+  validateOperationArguments() {
+    if (!this.querySelector || typeof this.querySelector !== "string")
+      throw new Error(
+        `OpenLinks operation must be provided with a querySelector.`
+      );
+  }
+
+  /**
+   *
+   * @param {{url:string,html:string}} params
+   * @return {Promise<{type:string,name:string,data:[]}>}
+   */
+  async scrape({ url, html }) {
+    if (!this.pageHelper) this.initPageHelper();
+    // debugger;
+    const refs = await this.createLinkList(html, url);
+
+    const hasOpenLinksOperation =
+      this.operations.filter((child) => child.constructor.name === "OpenLinks")
+        .length > 0; //Checks if the current page operation has any other page operations in it. If so, will force concurrency limitation.
+    let forceConcurrencyLimit = false;
+    if (hasOpenLinksOperation) {
+      forceConcurrencyLimit = 3;
+    }
+    // debugger;
+    const shouldPaginate = this.config.pagination ? true : false;
+    const iterations = [];
+
+    await mapPromisesWithLimitation(
+      refs,
+      async (href) => {
+        // debugger;
+        const data = await this.pageHelper.processOneIteration(
+          this.transformHref(href),
+          shouldPaginate,
+          false // ! Do not scrape children
+        );
+
+        iterations.push(data);
+      },
+      forceConcurrencyLimit
+        ? forceConcurrencyLimit
+        : this.scraper.config.concurrency
+    );
+
+    this.data.push(...iterations);
+    return {
+      type: this.constructor.name,
+      name: this.config.name,
+      data: iterations,
+    };
+  }
+
+  async createLinkList(html, url) {
+    // debugger;
+    var $ = cheerio.load(html);
+    // debugger;
+    const elementList = await createElementList($, this.querySelector, {
+      condition: this.config.condition,
+      slice: this.config.slice,
+    });
+    if (this.config.getElementList) {
+      await this.config.getElementList(elementList);
+    }
+    const baseUrlFromBaseTag = getBaseUrlFromBaseTag(
+      $,
+      this.scraper.config.baseSiteUrl
+    );
+
+    const refs = [];
+    elementList.forEach((link) => {
+      const absoluteUrl = getAbsoluteUrl(
+        baseUrlFromBaseTag || url,
+        link[0].attribs.href
+      );
+      refs.push(absoluteUrl);
+    });
+
+    return refs;
+  }
+}
+
+Object.assign(OpenLinks.prototype, CompositeInjectMixin);
+Object.assign(OpenLinks.prototype, CompositeScrapeMixin);
+
+module.exports = CollectLinks;
--- operations/helpers/PageHelper.js
+++ operations/helpers/PageHelper.js
@@ -1,186 +1,179 @@
 // const Operation = require('../Operation');//For jsdoc
-const { request } = require('../../request/request.js');
-const { stripTags } = require('../../utils/html');
-const { mapPromisesWithLimitation } = require('../../utils/concurrency');
-const { getDictionaryKey } = require('../../utils/objects');
-const { getPaginationUrls } = require('../../utils/pagination');
+const { request } = require("../../request/request.js");
+const { stripTags } = require("../../utils/html");
+const { mapPromisesWithLimitation } = require("../../utils/concurrency");
+const { getDictionaryKey } = require("../../utils/objects");
+const { getPaginationUrls } = require("../../utils/pagination");
 // const { CustomResponse } = require('../../request/request')//For jsdoc
 // const SPA_page = require('../../limitedSpa/SPA_Page');
 
-// require('../typedef.js'); 
+// require('../typedef.js');
 
 class PageHelper {
+  /**
+   *
+   * @param {Operation} Operation
+   */
+  constructor(Operation) {
+    this.Operation = Operation;
+  }
 
-    /**
-     * 
-     * @param {Operation} Operation 
-     */
-    constructor(Operation) {
-        this.Operation = Operation;
+  /**
+   *
+   * @param {string} href
+   * @param {boolean} shouldPaginate
+   * @return {Promise<{data:[],address:string}>}
+   */
+  async processOneIteration(
+    href,
+    shouldPaginate,
+    shouldScrapeChildren = false
+  ) {
+    //Will process one scraping object, including a pagination object. Used by Root and OpenLinks.
+    // debugger;
+    if (shouldPaginate) {
+      //If the scraping object is actually a pagination one, a different function is called.
+      return this.paginate(href);
     }
 
+    try {
+      var iteration = {
+        address: href,
+        data: [],
+      };
 
+      var response = await this.getPage(href);
+      // debugger
+      await this.runAfterResponseHooks(response);
 
+      // debugger;
+      if (shouldScrapeChildren)
+        var dataFromChildren = await this.Operation.scrapeChildren(
+          this.Operation.operations,
+          { html: response.data, url: response.url }
+        );
 
-    /**
-    * 
-    * @param {string} href      
-    * @param {boolean} shouldPaginate   
-    * @return {Promise<{data:[],address:string}>}   
-    */
-    async processOneIteration(href, shouldPaginate) {//Will process one scraping object, including a pagination object. Used by Root and OpenLinks.
+      await this.runGetPageObjectHook(href, dataFromChildren);
+
+      iteration.data = dataFromChildren;
+    } catch (error) {
+      // this.Operation.scraper.log(error)
+      // debugger;
+      const errorString = `There was an error opening page ${href}, ${error}`;
+      iteration.error = errorString;
+      iteration.successful = false;
+      this.Operation.errors.push(errorString);
+      this.Operation.handleFailedScrapingIteration(errorString);
+    } finally {
+      return iteration;
+    }
+  }
+
+  /**
+   *
+   * @param {string} address
+   * @return {Promise<string[]>} paginationUrls
+   */
+  async paginate(address) {
+    //Divides a given page to multiple pages.
+    const paginationConfig = this.Operation.config.pagination;
+    const paginationUrls = getPaginationUrls(address, paginationConfig);
+
+    const dataFromChildren = [];
+
+    await mapPromisesWithLimitation(
+      paginationUrls,
+      async (url) => {
+        const data = await this.processOneIteration(url, false);
+
+        dataFromChildren.push(data);
+      },
+      3
+    ); //The argument 3 forces lower promise limitation on pagination.
+    // return dataFromChildren;
+    return {
+      address: address,
+      data: dataFromChildren,
+    };
+  }
+
+  /**
+   *
+   * @param {string} href
+   * @return {Promise<string>}
+   */
+  async getPage(href) {
+    //Fetches the html of a given page.
+
+    const promiseFactory = async () => {
+      await this.Operation.beforePromiseFactory("Opening page:" + href);
+
+      let resp;
+      try {
+        resp = await request({
+          method: "get",
+          url: href,
+          timeout: this.Operation.scraper.config.timeout,
+          auth: this.Operation.scraper.config.auth,
+          headers: this.Operation.scraper.config.headers,
+          proxy: this.Operation.scraper.config.proxy,
+        });
+        if (this.Operation.scraper.config.removeStyleAndScriptTags) {
+          resp.data = stripTags(resp.data);
+        }
+
+        if (this.Operation.config.getPageHtml) {
+          // debugger;
+          await this.Operation.config.getPageHtml(resp.data, resp.url);
+        }
+      } catch (error) {
         // debugger;
-        if (shouldPaginate) {//If the scraping object is actually a pagination one, a different function is called. 
-            return this.paginate(href);
-        }
+        throw error;
+      } finally {
+        this.Operation.afterPromiseFactory();
+      }
+      return resp;
+    };
 
-        try {
+    return await this.Operation.qyuFactory(() =>
+      this.Operation.repeatPromiseUntilResolved(promiseFactory, href)
+    );
+  }
 
-            var iteration = {
-                address: href,
-                data: []
-            }
+  /**
+   *
+   * @param {string} address
+   * @param {Array} dataFromChildren
+   */
+  async runGetPageObjectHook(address, dataFromChildren) {
+    if (this.Operation.config.getPageObject) {
+      // debugger;
 
-            var response = await this.getPage(href);
-            // debugger
-            await this.runAfterResponseHooks(response)
-
-            // debugger;
-            var dataFromChildren = await this.Operation.scrapeChildren(this.Operation.operations, {html:response.data,url:response.url})
-
-            await this.runGetPageObjectHook(href, dataFromChildren)
-
-            iteration.data = dataFromChildren
-        }
-        catch (error) {
-            // this.Operation.scraper.log(error)
-            // debugger;
-            const errorString = `There was an error opening page ${href}, ${error}`;
-            iteration.error = errorString;
-            iteration.successful = false;
-            this.Operation.errors.push(errorString);
-            this.Operation.handleFailedScrapingIteration(errorString);
-        } finally {
-            return iteration
-        }
+      const tree = {};
+      for (let child of dataFromChildren) {
+        // debugger;
+        // tree[child.name] = child.data
+        const func = getDictionaryKey(child.name);
+        tree[func(child.name, tree)] = child.data;
+      }
+      await this.Operation.config.getPageObject(tree, address);
     }
+  }
 
-
-
-    /**
-     * 
-     * @param {string} address 
-     * @return {Promise<string[]>} paginationUrls
-     */
-    async paginate(address) {//Divides a given page to multiple pages.
-        const paginationConfig = this.Operation.config.pagination;
-        const paginationUrls = getPaginationUrls(address, paginationConfig)
-
-
-        const dataFromChildren = [];
-
-        await mapPromisesWithLimitation(paginationUrls, async (url) => {
-            const data = await this.processOneIteration(url, false);
-
-            dataFromChildren.push(data);
-
-        }, 3);//The argument 3 forces lower promise limitation on pagination.
-        // return dataFromChildren;
-        return {
-            address: address,
-            data: dataFromChildren
-        }
+  /**
+   *
+   * @param {CustomResponse} response
+   * @return {Promise<void>}
+   */
+  async runAfterResponseHooks(response) {
+    if (this.Operation.config.getPageResponse) {
+      //If a "getResponse" callback was provided, it will be called
+      // debugger;
+      if (typeof this.Operation.config.getPageResponse !== "function")
+        throw "'getPageResponse' callback must be a function";
+      await this.Operation.config.getPageResponse(response);
     }
-
-
-    /**
-     * 
-     * @param {string} href 
-     * @return {Promise<string>}
-     */
-    async getPage(href) {//Fetches the html of a given page.
-
-        const promiseFactory = async () => {
-
-            await this.Operation.beforePromiseFactory('Opening page:' + href);
-
-            let resp;
-            try {
-
-                resp = await request({
-                    method: 'get', url: href,
-                    timeout: this.Operation.scraper.config.timeout,
-                    auth: this.Operation.scraper.config.auth,
-                    headers: this.Operation.scraper.config.headers,
-                    proxy: this.Operation.scraper.config.proxy
-
-                })
-                if (this.Operation.scraper.config.removeStyleAndScriptTags) {
-                    resp.data = stripTags(resp.data);
-                }
-
-                if (this.Operation.config.getPageHtml) {
-                    // debugger;
-                    await this.Operation.config.getPageHtml(resp.data, resp.url)
-                }
-
-            } catch (error) {
-                // debugger;
-                throw error;
-            }
-            finally {
-                this.Operation.afterPromiseFactory();
-            }
-            return resp;
-        }
-
-        return await this.Operation.qyuFactory(() => this.Operation.repeatPromiseUntilResolved(promiseFactory, href));
-    }
-
-
-    /**
-     * 
-     * @param {string} address 
-     * @param {Array} dataFromChildren 
-     */
-    async runGetPageObjectHook(address, dataFromChildren) {
-        if (this.Operation.config.getPageObject) {
-            // debugger;
-
-            const tree = {
-                
-            }
-            for (let child of dataFromChildren) {
-                // debugger;
-                // tree[child.name] = child.data
-                const func = getDictionaryKey(child.name);
-                tree[func(child.name, tree)] = child.data
-            }
-            await this.Operation.config.getPageObject(tree,address)
-        }
-    }
-
-
-
-
-
-    /**
-     * 
-     * @param {CustomResponse} response 
-     * @return {Promise<void>}
-     */
-    async runAfterResponseHooks(response) {
-        if (this.Operation.config.getPageResponse) {//If a "getResponse" callback was provided, it will be called
-            // debugger;
-            if (typeof this.Operation.config.getPageResponse !== 'function')
-                throw "'getPageResponse' callback must be a function";
-            await this.Operation.config.getPageResponse(response);
-
-        }
-    }
-
-
-
+  }
 }
 
 module.exports = PageHelper;
